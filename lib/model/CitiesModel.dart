f20abcb9f9c96eab559b7758d9a4bf078db088d1
jhanvi <jhanvipathar19@gmail.com>
2022-01-19 01:32:48 +0530

    Leaving From changes

---
+++ lib/model/CitiesModel.dart
@@ -0,0 +1,70 @@
+class CitiesList {
+  List<FromBuses>? fromBuses;
+  List<ToBuses>? toBuses;
+
+  CitiesList({this.fromBuses, this.toBuses});
+
+  CitiesList.fromJson(Map<String, dynamic> json) {
+    if (json['From_Buses'] != null) {
+      fromBuses = <FromBuses>[];
+      json['From_Buses'].forEach((v) {
+        fromBuses!.add(new FromBuses.fromJson(v));
+      });
+    }
+    if (json['To_Buses'] != null) {
+      toBuses = <ToBuses>[];
+      json['To_Buses'].forEach((v) {
+        toBuses!.add(new ToBuses.fromJson(v));
+      });
+    }
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    if (this.fromBuses != null) {
+      data['From_Buses'] = this.fromBuses!.map((v) => v.toJson()).toList();
+    }
+    if (this.toBuses != null) {
+      data['To_Buses'] = this.toBuses!.map((v) => v.toJson()).toList();
+    }
+    return data;
+  }
+}
+
+class FromBuses {
+  String? id;
+  String? status;
+  String? name;
+
+  FromBuses({this.id,this.status, this.name});
+
+  FromBuses.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    name = json['name'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['id'] = this.id;
+    data['name'] = this.name;
+    return data;
+  }
+}
+class ToBuses {
+  String? id;
+  String? name;
+
+  ToBuses({this.id, this.name});
+
+  ToBuses.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    name = json['name'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['id'] = this.id;
+    data['name'] = this.name;
+    return data;
+  }
+}
--- lib/screens/home/bookAtripTab/book_a_trip_tab.dart
+++ lib/screens/home/bookAtripTab/book_a_trip_tab.dart
@@ -1,10 +1,13 @@
+import 'dart:convert';
 import 'package:buenos_transportation/globalWidgets/textWidget/text_widget.dart';
+import 'package:buenos_transportation/model/CitiesModel.dart';
 import 'package:buenos_transportation/responsiveBloc/size_config.dart';
 import 'package:buenos_transportation/screens/home/bookAtripTab/Widgets/discount_dropdown.dart';
 import 'package:buenos_transportation/screens/home/bookAtripTab/selectDeparture/select_departure.dart';
 import 'package:buenos_transportation/theme/theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'package:http/http.dart' as http;
 
 class BookATripTab extends StatefulWidget {
   const BookATripTab({Key? key}) : super(key: key);
@@ -14,6 +17,19 @@ class BookATripTab extends StatefulWidget {
 }
 
 class _BookATripTabState extends State<BookATripTab> {
+
+  @override
+  void initState() {
+    super.initState();
+    fetchToAndFromCities();
+  }
+
+  var fromCitiesJson;
+  var toCitiesJson;
+  int cityFromSelectedId = 0;
+  int cityToSelectedId = 0;
+  var cityFromSelectedName ;
+  var cityToSelectedName ;
   DateTime _dateTime = DateTime.now();
   DateTime _returnDateTime = DateTime.now();
   bool addReturnTripSelected = false;
@@ -68,36 +84,48 @@ class _BookATripTabState extends State<BookATripTab> {
   Widget leavingFromField(screenWidth) {
     return Container(
       height: screenWidth * 12.5,
-      padding: EdgeInsets.only(left: screenWidth * 3),
+      padding: EdgeInsets.only(left: screenWidth * 3, right: screenWidth * 3),
       margin: EdgeInsets.only(bottom: screenWidth * 3),
+      width: double.infinity,
       decoration: BoxDecoration(
           color: Colors.white,
           borderRadius: BorderRadius.circular(screenWidth * 3)),
-      child: TextField(
-        style:
-            TextStyle(color: Colors.grey.shade800, fontSize: screenWidth * 3.5),
-        decoration: InputDecoration(
-            border: InputBorder.none,
-            suffixIcon: GestureDetector(
-              onTap: () {
-                Navigator.of(context).push(
-                    MaterialPageRoute(builder: (context) => SelectDeparture()));
-              },
-              child: SizedBox(
-                height: screenWidth * 1,
+      child: DropdownButtonHideUnderline(
+          child: DropdownButton(
+              icon: SizedBox(
+                height: screenWidth * 3,
                 width: screenWidth * 3,
                 child: Image.asset(
                   'assets/location_icon.png',
                   fit: BoxFit.contain,
                 ),
               ),
-            ),
-            suffixIconConstraints: BoxConstraints(
-                minWidth: screenWidth * 10, minHeight: screenWidth * 4),
-            hintText: 'Leaving From',
-            hintStyle: TextStyle(
-                color: Colors.grey.shade400, fontSize: screenWidth * 3.5)),
-      ),
+              isExpanded: true,
+              style: TextStyle(color: Colors.grey.shade900, fontSize: 13),
+              hint: Text(
+                  cityFromSelectedId == 0
+                      ? 'Leaving From'
+                      : cityFromSelectedName,
+                  style: TextStyle(
+                      color: cityFromSelectedId == 0
+                          ? Colors.grey.shade400
+                          : Colors.grey.shade800,
+                      fontSize: screenWidth * 3.5)),
+              onChanged: (val) {
+                setState(() {
+                  cityFromSelectedId =  int.parse(val.toString());
+                  cityFromSelectedName = val;
+                });
+              },
+            value: cityFromSelectedName,
+
+            items: fromCitiesJson?.map<DropdownMenuItem>((item) {
+              return new DropdownMenuItem(
+                child: new Text(item['name']),
+                value: item['id'].toString(),
+              );
+            }).toList()?? [],
+          )),
     );
   }
 
@@ -110,7 +138,7 @@ class _BookATripTabState extends State<BookATripTab> {
           borderRadius: BorderRadius.circular(screenWidth * 3)),
       child: TextField(
         style:
-            TextStyle(color: Colors.grey.shade800, fontSize: screenWidth * 3.5),
+        TextStyle(color: Colors.grey.shade800, fontSize: screenWidth * 3.5),
         decoration: InputDecoration(
             border: InputBorder.none,
             suffixIcon: SizedBox(
@@ -139,141 +167,145 @@ class _BookATripTabState extends State<BookATripTab> {
         children: [
           Expanded(
               child: GestureDetector(
-            onTap: () async {
-              dateSelected = true;
-              DateTime? dateTime;
-              dateTime = await showDatePicker(
-                context: context,
-                initialEntryMode: DatePickerEntryMode.calendar,
-                initialDate: _dateTime,
-                firstDate: DateTime(2015),
-                lastDate: DateTime(2025),
-              );
-              if (dateTime != null) {
-                setState(() {
-                  _dateTime = dateTime!;
-                });
-              }
-            },
-            child: Container(
-                height: screenWidth * 12.5,
-                padding: EdgeInsets.symmetric(horizontal: screenWidth * 3),
-                margin:
+                onTap: () async {
+                  dateSelected = true;
+                  DateTime? dateTime;
+                  dateTime = await showDatePicker(
+                    context: context,
+                    initialEntryMode: DatePickerEntryMode.calendar,
+                    initialDate: _dateTime,
+                    firstDate: DateTime(2015),
+                    lastDate: DateTime(2025),
+                  );
+                  if (dateTime != null) {
+                    setState(() {
+                      _dateTime = dateTime!;
+                    });
+                  }
+                },
+                child: Container(
+                    height: screenWidth * 12.5,
+                    padding: EdgeInsets.symmetric(horizontal: screenWidth * 3),
+                    margin:
                     EdgeInsets.only(top: screenWidth * 3, right: screenWidth),
-                decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(screenWidth * 3)),
-                child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        '${monthNames[_dateTime.month - 1]} ${_dateTime.day},${_dateTime.year}',
-                        style: TextStyle(
-                            fontSize: screenWidth * 3.3,
-                            color: !dateSelected
-                                ? Colors.grey.shade400
-                                : Colors.grey.shade800),
-                      ),
-                      SizedBox(
-                        height: screenWidth * 3.8,
-                        width: screenWidth * 3.8,
-                        child: Image.asset(
-                          'assets/calendar_icon.png',
-                          fit: BoxFit.contain,
-                        ),
-                      )
-                    ])),
-          )),
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(screenWidth * 3)),
+                    child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            '${monthNames[_dateTime.month - 1]} ${_dateTime
+                                .day},${_dateTime.year}',
+                            style: TextStyle(
+                                fontSize: screenWidth * 3.3,
+                                color: !dateSelected
+                                    ? Colors.grey.shade400
+                                    : Colors.grey.shade800),
+                          ),
+                          SizedBox(
+                            height: screenWidth * 3.8,
+                            width: screenWidth * 3.8,
+                            child: Image.asset(
+                              'assets/calendar_icon.png',
+                              fit: BoxFit.contain,
+                            ),
+                          )
+                        ])),
+              )),
           Expanded(
               child: Container(
-            child: Stack(
-              children: [
-                Align(
-                    alignment: Alignment.bottomCenter,
-                    child: GestureDetector(
+                child: Stack(
+                  children: [
+                    Align(
+                        alignment: Alignment.bottomCenter,
+                        child: GestureDetector(
+                          onTap: () {
+                            setState(() {
+                              addReturnTripSelected = true;
+                            });
+                          },
+                          child: Container(
+                              height: screenWidth * 12.5,
+                              padding:
+                              EdgeInsets.symmetric(horizontal: screenWidth * 3),
+                              margin: EdgeInsets.only(
+                                  top: screenWidth * 3, left: screenWidth),
+                              decoration: BoxDecoration(
+                                  color: Colors.white,
+                                  borderRadius:
+                                  BorderRadius.circular(screenWidth * 3)),
+                              child: Row(
+                                  mainAxisAlignment: MainAxisAlignment
+                                      .spaceBetween,
+                                  children: [
+                                    Text(
+                                      !addReturnTripSelected
+                                          ? 'Add return trip'
+                                          : '${monthNames[_returnDateTime
+                                          .month - 1]} ${_returnDateTime
+                                          .day},${_returnDateTime.year}',
+                                      style: TextStyle(
+                                          fontSize: screenWidth * 3.3,
+                                          color: !addReturnTripSelected
+                                              ? Colors.grey.shade400
+                                              : Colors.grey.shade800),
+                                    ),
+                                    !addReturnTripSelected
+                                        ? SizedBox()
+                                        : GestureDetector(
+                                      onTap: () async {
+                                        DateTime? dateTime;
+                                        dateTime = await showDatePicker(
+                                          context: context,
+                                          initialEntryMode:
+                                          DatePickerEntryMode.calendar,
+                                          initialDate: _returnDateTime,
+                                          firstDate: DateTime(2015),
+                                          lastDate: DateTime(2025),
+                                        );
+                                        if (dateTime != null) {
+                                          setState(() {
+                                            _returnDateTime = dateTime!;
+                                          });
+                                        }
+                                      },
+                                      child: SizedBox(
+                                        height: screenWidth * 3.8,
+                                        width: screenWidth * 3.8,
+                                        child: Image.asset(
+                                          'assets/calendar_icon.png',
+                                          fit: BoxFit.contain,
+                                        ),
+                                      ),
+                                    )
+                                  ])),
+                        )),
+                    !addReturnTripSelected
+                        ? SizedBox()
+                        : GestureDetector(
                       onTap: () {
                         setState(() {
-                          addReturnTripSelected = true;
+                          addReturnTripSelected = false;
                         });
                       },
                       child: Container(
-                          height: screenWidth * 12.5,
-                          padding:
-                              EdgeInsets.symmetric(horizontal: screenWidth * 3),
-                          margin: EdgeInsets.only(
-                              top: screenWidth * 3, left: screenWidth),
-                          decoration: BoxDecoration(
-                              color: Colors.white,
-                              borderRadius:
-                                  BorderRadius.circular(screenWidth * 3)),
-                          child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                Text(
-                                  !addReturnTripSelected
-                                      ? 'Add return trip'
-                                      : '${monthNames[_returnDateTime.month - 1]} ${_returnDateTime.day},${_returnDateTime.year}',
-                                  style: TextStyle(
-                                      fontSize: screenWidth * 3.3,
-                                      color: !addReturnTripSelected
-                                          ? Colors.grey.shade400
-                                          : Colors.grey.shade800),
-                                ),
-                                !addReturnTripSelected
-                                    ? SizedBox()
-                                    : GestureDetector(
-                                        onTap: () async {
-                                          DateTime? dateTime;
-                                          dateTime = await showDatePicker(
-                                            context: context,
-                                            initialEntryMode:
-                                                DatePickerEntryMode.calendar,
-                                            initialDate: _returnDateTime,
-                                            firstDate: DateTime(2015),
-                                            lastDate: DateTime(2025),
-                                          );
-                                          if (dateTime != null) {
-                                            setState(() {
-                                              _returnDateTime = dateTime!;
-                                            });
-                                          }
-                                        },
-                                        child: SizedBox(
-                                          height: screenWidth * 3.8,
-                                          width: screenWidth * 3.8,
-                                          child: Image.asset(
-                                            'assets/calendar_icon.png',
-                                            fit: BoxFit.contain,
-                                          ),
-                                        ),
-                                      )
-                              ])),
-                    )),
-                !addReturnTripSelected
-                    ? SizedBox()
-                    : GestureDetector(
-                        onTap: () {
-                          setState(() {
-                            addReturnTripSelected = false;
-                          });
-                        },
-                        child: Container(
-                          margin: EdgeInsets.only(top: 3),
-                          height: screenWidth * 5,
-                          width: screenWidth * 5,
-                          padding: EdgeInsets.all(screenWidth * 1.4),
-                          alignment: Alignment.center,
-                          decoration: BoxDecoration(
-                              color: theme.redColor, shape: BoxShape.circle),
-                          child: Image.asset(
-                            'assets/close_icon.png',
-                            color: Colors.white,
-                          ),
+                        margin: EdgeInsets.only(top: 3),
+                        height: screenWidth * 5,
+                        width: screenWidth * 5,
+                        padding: EdgeInsets.all(screenWidth * 1.4),
+                        alignment: Alignment.center,
+                        decoration: BoxDecoration(
+                            color: theme.redColor, shape: BoxShape.circle),
+                        child: Image.asset(
+                          'assets/close_icon.png',
+                          color: Colors.white,
                         ),
-                      )
-              ],
-            ),
-          ))
+                      ),
+                    )
+                  ],
+                ),
+              ))
         ],
       ),
     );
@@ -289,7 +321,7 @@ class _BookATripTabState extends State<BookATripTab> {
           borderRadius: BorderRadius.circular(screenWidth * 3)),
       child: TextField(
         style:
-            TextStyle(color: Colors.grey.shade800, fontSize: screenWidth * 3.5),
+        TextStyle(color: Colors.grey.shade800, fontSize: screenWidth * 3.5),
         decoration: InputDecoration(
             border: InputBorder.none,
             suffixIcon: SizedBox(
@@ -345,5 +377,21 @@ class _BookATripTabState extends State<BookATripTab> {
           child: text('SEARCH', screenWidth * 4, Colors.white,
               fontWeight: FontWeight.w500)),
     );
+  }
+
+  void fetchToAndFromCities() async {
+    var response = await http
+        .get(Uri.parse('https://jsonkeeper.com/b/74GJ'));
+
+    if (response.statusCode == 200) {
+      var jsonData = jsonDecode(response.body);
+
+      fromCitiesJson = jsonData["From_Buses"];
+      toCitiesJson = jsonData["To_Buses"];
+
+    }
+    else{
+      AlertDialog("Connection Error!");
+    }
   }
 }
